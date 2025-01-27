2823eeb2610477338e4e0a4765cc9028d45f111b
Dashi <dorzhiev_dd@mail.ru>
2025-01-27 23:44:10 +1200

    Сжатие картинки размером 1.8 мб

--- Home_work_21.py
+++ Home_work_21.py
@@ -1,2 +1,21 @@
 from PIL import Image
 from pillow_heif import register_heif_opener, from_pillow as heif_from_pillow
+
+sauna = r"C:\Users\Admin\Pictures\1\sauna.jpg"
+
+register_heif_opener()
+
+source_image = Image.open(sauna)
+
+source_image.save(
+    "output.webp", 
+    format="WEBP", 
+    quality=40
+)
+
+
+heif_file = heif_from_pillow(source_image)
+heif_file.save(
+    "output.heic", 
+    quality=40
+)
