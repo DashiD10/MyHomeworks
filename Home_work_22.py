2b5842dbb7a1059541bd265e4ca54a5950293369
Dashi <dorzhiev_dd@mail.ru>
2025-02-04 22:44:11 +1200

    Функция для чтения текстового файла

--- Home_work_22.py
+++ Home_work_22.py
@@ -4,8 +4,15 @@ import yaml
 
 
 def read_txt (file_path: str, encoding: str = "utf-8") -> list[str]:
-    
-    pass
+    """
+    Функция читает файл 
+
+    :param file_path: путь к файлу
+    :param encoding: кодировка файла
+    :return: список строк из файла
+    """
+    with open(file_path, "r", encoding=encoding) as file:
+        return file.readlines()
 
 def read_csv (file_path: str, encoding: str = "utf-8") -> None:
     pass
