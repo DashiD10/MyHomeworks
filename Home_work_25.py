e95890825dc86f3dba76544f1f2cf2159bfde2a4
Dashi <dorzhiev_dd@mail.ru>
2025-03-05 08:36:20 +1200

    Метод file_path

--- Home_work_25.py
+++ Home_work_25.py
@@ -1,10 +1,15 @@
+from typing import Tuple
+
 class TxtHandler:
         """
     Класс для работы с текстовыми файлами
     Methods:
         1. read() ->List[str]: возвращает список строк из файла
-        2. write(data: List[str]) -> None: записывает список строк в файл
-        3. append(data: List[str]) -> None: добавляет список строк в конец Exceptions:
+        2. write(*data: Tuple[str, ...]) -> None: записывает список строк в файл
+        3. append(*data: Tuple[str, ...]) -> None: добавляет список строк в конец Exceptions:
     FileNotFoundError: если файл не найден
     PermissionError: если нет прав на запись
         """
+
+    def __init__(self, file_path: str) -> None:
+            self.file_path = file_path
