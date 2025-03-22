5aaf7f64e0a2afcf75a7c3bf791d112ba6421a1c
Dashi <dorzhiev_dd@mail.ru>
2025-03-22 13:01:13 +1200

    Инициализация ImageCompressor, геттер и сеттер

--- Home_work_26.py
+++ Home_work_26.py
@@ -18,3 +18,38 @@ class ImageCompressor:
     """
     
     supported_formats: Tuple[str, ...] = ('.jpg', '.jpeg', '.png')
+
+    def __init__(self, quality: int = 50):
+        """
+        Инициализирует объект ImageCompressor.
+        
+        Args:
+            quality (int): Качество сжатия изображения (1-100).
+                По умолчанию 50.
+        """
+        self.__quality = quality
+        register_heif_opener()
+    
+    @property
+    def quality(self) -> int:
+        """
+        Геттер для получения текущего значения качества сжатия.
+        
+        Returns:
+            int: Текущее значение качества сжатия.
+        """
+        return self.__quality
+    
+    @quality.setter
+    def quality(self, quality: int) -> None:
+        """
+        Сеттер для установки значения качества сжатия.
+        
+        Args:
+            quality (int): Новое значение качества сжатия (1-100).
+        """
+        if not isinstance(quality, int):
+            raise TypeError("Качество должно быть целым числом")
+        if quality < 1 or quality > 100:
+            raise ValueError("Качество должно быть в диапазоне от 1 до 100")
+        self.__quality = quality
